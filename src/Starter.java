de028bf8f16a0b057ada3413c54d0c677d203922
Leonardo S <l01.s@g.com>
2020-06-03 11:08:22 +0200

    Updated Starter

--- src/Starter.java
+++ src/Starter.java
@@ -1,9 +1,25 @@
+import java.util.ArrayList;
 
 public class Starter {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		ArrayList<Character> p1c = new ArrayList<Character>();
+		ArrayList<Character> p2c = new ArrayList<Character>();
+		
+		p1c.add(new Colonel(new int[] {4, 5}));
+		p1c.add(new General(new int[] {5, 5}));
+		
+		p2c.add(new Colonel(new int[] {5, 4}));
+		p2c.add(new General(new int[] {4, 4}));
+		
+		Player p1 = new Player("player1", p1c);
+		Player p2 = new Player("player2", p2c);
+		
+		//Game Class
+		
+		System.out.println(p1.getCharacterByPosition(new int[] {5, 4}).move(p1, p2, 2));
 	}
 
 }
